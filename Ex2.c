7af409c7100ad3b45a0ba000bce38e4357f46811
LopezSanchezGustavo <82688576+LopezSanchezGustavo@users.noreply.github.com>
2021-05-12 21:30:28 -0500

    EXAMEN SEGUNDO PARCIAL
    Estos son los ejercicios del examen de segundo parcial

---
+++ Ex2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    /* Gustavo Lopez sanchez
+    equipo Alfa Buena Maravilla 12 de mayo */
+int main () {
+    int a, b, c;
+    printf ("\n \n \n \nPrograma 2 \n");
+    printf ("\n Gustavo Lopez Sanchez\n");
+    printf ("Suma de numero enteros \n");
+    printf ("\n \n Dame el valor entero de a:");
+    scanf ("%d",&a);
+    printf ("\n \nDame el valor entero de b:");
+    scanf ("%d",&b);
+    c = a + b;
+    printf ("\n El resultado es %d",c);
+    return 0;
+}
